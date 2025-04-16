6f063695211d250fd97afbe2ce2db9b1fee61f01
master <master@SMLAB306PC19_25>
2025-04-15 21:38:40 -0500

    Agregar archivos de proyecto.

---
+++ Views/vElementos.xaml.cs
@@ -0,0 +1,38 @@
+namespace aburbanoS2A.Views;
+
+public partial class vElementos : ContentPage
+{
+	public vElementos()
+	{
+		InitializeComponent();
+	}
+
+    private void Paises_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			if (pkPaises.SelectedIndex == -1)
+			{
+				DisplayAlert("Error", "El dato no exixte", "Cerrar");
+				
+			} else
+				{
+					string pais = pkPaises.Items[pkPaises.SelectedIndex].ToString();
+				DisplayAlert("Paises", "El pais seleccionado es:" + pais, "Cerrar");
+
+				}
+		}
+		catch (Exception ex)
+		{
+
+			DisplayAlert("Error", ex.Message, "Cerrar");
+		}
+		
+    }
+
+    private void btnFecha_Clicked(object sender, EventArgs e)
+    {
+		string  date = dpfecha.Date.ToString();
+		DisplayAlert("Error", "La fecha es: " + date, "Cerrar");
+    }
+}
